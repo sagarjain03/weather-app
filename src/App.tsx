f7b44eaf8ed06434a6c483ede25403f18c4339bf
sagarjain03 <141217003+sagarjain03@users.noreply.github.com>
2025-06-18 02:45:38 +0530

    api endpoints created

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Layout from "./components/Layout"
-import { ThemeProvider } from "./context/theme-provider"
-import WeatherPage from "./pages/weatherPage"
-import CityPage from "./pages/cityPage"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherPage from "./pages/weatherPage";
+import CityPage from "./pages/cityPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const App = () => {
+  const queryClient = new QueryClient();
   return (
     <div>
-  <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Layout>
+              <Routes>
+                <Route path="/" element={<WeatherPage />} />
+                <Route path="/city/:cityName" element={<CityPage />} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
 
-  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-   <Layout>
-    <Routes>
-      <Route path="/" element={< WeatherPage/>} />
-      <Route path="/city/:cityName" element={<CityPage />} />
-    </Routes>
-   </Layout>
-
-
-  </ThemeProvider>
-  </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
